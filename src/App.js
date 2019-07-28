d6188c7b490c6a66f1e6d32d6394c931f678ed4d
Florent Xing Wenfeng <florentxingwenfeng@gmail.com>
2019-07-28 10:03:21 -0400

    add the sub components

--- src/App.js
+++ src/App.js
@@ -4,18 +4,74 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: 'Adam'
+      username: 'Adam',
+      todoItems:[
+        {action: 'But Flowers', done: false},
+        {action: 'Get shoes', done: false},
+        {action: 'Collect Tickets', done: false},
+        {action: 'Call Joe', done: false}
+      ],
+      newItemText: '' 
     }
   }
-  render() {
-  return (
+
+  updateNewTextValue = (e) => {
+    this.setState({
+      newItemText: e.target.value
+    })
+  }
+
+  createNewTodo = () => {
+    if(!this.state.todoItems
+      .find(item => item.action === this.state.newItemText)) {
+        this.setState({
+          todoItems: [...this.state.todoItems, {
+            action: this.state.newItemText, done: false
+          }],
+          newItemText: ''
+        })
+      }
+  }
+
+  toggleTodo = (todo) => {
+    this.setState({
+      todoItems: this.state.todoItems.map(item => item.action === todo.action
+        ? {...item, done: !item.done} : item)
+    });
+  }
+
+  totoTableRows = () => this.state.todoItems.map(item => 
+    <tr key={item.action}>
+      <td>{item.action}</td>
+      <td>
+        <input type="checkbox" checked={ item.done } onChange={ () => this.toggleTodo(item) } />
+      </td>
+    </tr>)
+  render = () => 
     <div>
       <h4 className="bg-primary text-white text-center p-2">
         {this.state.username}'s To Do List
+        <br />
+        {this.state.todoItems.filter(t => !t.done).length} items todo
       </h4>
-    </div>
-  );
-  }
+      <div className="container-fluid">
+        <div className="my-1">
+          <input className="form-control"
+            value={this.state.newItemText}
+            onChange={ this.updateNewTextValue } />
+          <button className="btn btn-primary mt-1"
+            onClick={ this.createNewTodo }>
+            Add Item
+          </button>
+        </div>
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr><th>Description</th><th>Done</th></tr>
+          </thead>
+          <tbody>{ this.totoTableRows() }</tbody>
+        </table>
+      </div>
+   </div>
 }
 
 
